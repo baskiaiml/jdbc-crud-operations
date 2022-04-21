13d6127c460e36c8ff479225bbe043cd4cad1584
skillprofile-user-at-417125717649 <baski.ciet@gmail.com>
2022-04-21 05:52:33 +0530

    new file changes

---
+++ src/main/java/com/stackroute/crud/SpringSampleApplication.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.stackroute.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 91994
+ *
+ */
+@SpringBootApplication
+public class SpringSampleApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSampleApplication.class, args);
+	}
+
+}
